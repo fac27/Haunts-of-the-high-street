2dd9062a77d6f0f6ce3ca798f10f1de1bcbfd972
cameochoquer <cjchoquer@gmail.com>
2023-05-31 12:44:45 +0100

    added sign up and login redirects

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
-const express = require("express");
-const home = require("./routes/home.js");
+const express = require('express')
+const home = require('./routes/home.js')
+const login = require('./routes/log-in.js')
+const signup = require('./routes/sign-up.js')
 
-
-const server = express();
+const server = express()
 
 server.use((req, res, next) => {
-  const time = new Date().toLocaleTimeString("en-GB");
-  console.log(`${time} ${req.method} ${req.url}`);
-  next();
-});
+  const time = new Date().toLocaleTimeString('en-GB')
+  console.log(`${time} ${req.method} ${req.url}`)
+  next()
+})
 
-server.get("/", home.get);
-module.exports = server;
+server.get('/', home.get)
+server.get('/log-in', login.get)
+server.get('/sign-up', signup.get)
+
+module.exports = server
