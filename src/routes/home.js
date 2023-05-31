a1b679421b634c95786667b052af59f9da808c80
cameochoquer <cjchoquer@gmail.com>
2023-05-31 16:49:05 +0100

    all pages have initial stylings now

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,12 +3,11 @@ const { Layout } = require('../templates/layout')
 const get = (req, res) => {
   const title = 'Haunts of the High Street'
   const content = /*html*/ `
-    <header>
-    <nav><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>
+    <header class="flex purple padding">
+    <h1 class="creepy">${title}</h1>
+    <nav class="end center"><a href="/sign-up">Sign up</a> or <a href="/log-in">log in</a></nav>
     </header>
-    <div class="red">
-      <h1 class="creepy">${title}</h1>
-    </div>
+
     <div>
     <p>user sighting</p>
     </div>
