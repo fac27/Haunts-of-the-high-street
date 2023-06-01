7877c57150b53c58061eab2a700c07aa22c59ee6
Taha <tahafhassan@gmail.com>
2023-06-01 15:31:47 +0100

    only delete when logged in

--- src/routes/deletePost.js
+++ src/routes/deletePost.js
@@ -1,8 +1,14 @@
 const { deleteSighting } = require("../model/sighting.js");
 
 const post = (req, res) => {
+    const session = req.session;
     const sighting_id = req.body.sighting;
-    deleteSighting(sighting_id)
+    if(session){
+     deleteSighting(sighting_id)   
+    } else {
+        console.log('Oops! Login or Sign-Up first!');
+    }
+    
     res.redirect('/')
 }
 
