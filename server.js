2009f9fbfd2dd3dadbd8757e032e6763eb4b4512
Mark hanley <home@markhanley.co.uk>
2023-06-01 14:52:23 +0100

    change double quotes to single in server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const cookieParser = require("cookie-parser");
-const express = require("express");
+const cookieParser = require('cookie-parser');
+const express = require('express');
 const server = express();
 
-const home = require("./src/routes/home.js");
-const login = require("./src/routes/log-in");
+const home = require('./src/routes/home.js');
+const login = require('./src/routes/log-in');
 const logout = require('./src/routes/log-out.js');
-const signup = require("./src/routes/sign-up.js");
-const {getSession, removeSession} = require("./src/model/session.js");
+const signup = require('./src/routes/sign-up.js');
+const { getSession, removeSession } = require('./src/model/session.js');
 
-const staticHandler = express.static("public");
-require("dotenv").config();
+const staticHandler = express.static('public');
+require('dotenv').config();
 server.use(staticHandler);
 
 const body = express.urlencoded({ extended: true });
 const cookies = cookieParser(process.env.COOKIE_SECRET);
 
 server.use((req, res, next) => {
-  const time = new Date().toLocaleTimeString("en-GB");
+  const time = new Date().toLocaleTimeString('en-GB');
   console.log(`${time} ${req.method} ${req.url}`);
   next();
 });
@@ -27,9 +27,10 @@ const sessions = (req, res, next) => {
   if (session) {
     const expiry = new Date(session.expires_at);
     const today = new Date();
-    if (expiry < today) {
+    if (expiry < today)
+    {
       removeSession(sid);
-      res.clearCookie("sid");
+      res.clearCookie('sid');
     } else {
       req.session = session;
     }
@@ -40,12 +41,12 @@ const sessions = (req, res, next) => {
 server.use(cookies);
 server.use(sessions);
 
-server.get("/", home.get);
-server.get("/sign-up", signup.get);
-server.post("/sign-up", body, signup.post);
-server.get("/log-in", login.get);
-server.post("/log-in", body, login.post);
-server.post("/", body, home.post);
-server.post('/log-out', body,  logout.post);
+server.get('/', home.get);
+server.get('/sign-up', signup.get);
+server.post('/sign-up', body, signup.post);
+server.get('/log-in', login.get);
+server.post('/log-in', body, login.post);
+server.post('/', body, home.post);
+server.post('/log-out', body, logout.post);
 
 module.exports = server;
