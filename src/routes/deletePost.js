301fc4f491077806354a0bada3de0c83f729facf
Taha <tahafhassan@gmail.com>
2023-06-01 16:28:45 +0100

    remove console logs

--- src/routes/deletePost.js
+++ src/routes/deletePost.js
@@ -3,16 +3,14 @@ const { deleteSighting } = require("../model/sighting.js");
 const post = (req, res) => {
     const session = req.session;
     if(!session) {
-        console.log('Oops! Login or Sign-Up first!');
-        //res.status(402).send(`<h1>Oops! Login or Sign-Up first!</h1>`)
+        res.status(402).send(`<h1>Oops! Login or Sign-Up first!</h1>`)
         return
     }
     const sighting_id = req.body.sighting_id;
     const isUser = sighting_id == session.user_id;
-    console.log(isUser);
+    // eslint-disable-next-line no-negated-condition
     if(!isUser){
-        console.log("That's not your post!")
-        //res.status(401).send(`<h1>That's not your post!</h1>`)
+        res.status(401).send(`<h1>That's not your post!</h1>`)
         return
     } else {
        deleteSighting(sighting_id)    
